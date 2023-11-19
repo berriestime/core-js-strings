f44ed1dd46a59e54c1c71cbd5680e72c3fe5fe52
Clukva <berries.whale@gmail.com>
2023-11-19 20:33:10 +0300

    feat: add isString function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -26,7 +26,6 @@ function getStringLength(value) {
   if (value === null || value === undefined || Number.isNaN(value)) {
     return 0;
   }
-
   return value.length;
 }
 
@@ -44,8 +43,8 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 /**
